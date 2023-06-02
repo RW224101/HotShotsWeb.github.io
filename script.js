14c14d1a93815de344589bd516999f390e240494
RW224101 <135328734+RW224101@users.noreply.github.com>
2023-06-02 20:23:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-const cookieBox = document.querySelector(".wrapper"),
-    buttons = document.querySelectorAll(".button"),
-
-const executeCodes = () => {
-    cookieBox.classList.add("show")
-};
-
-window.addEventListener("load", executeCodes)
